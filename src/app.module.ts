4b918b30630c6ae08f7bc0551139dec4e8b67142
maximedervaux <maximeder.pro@gmail.com>
2024-07-30 09:26:03 +0200

    .env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,37 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+
 import { MangaModule } from './manga/manga.module';
 import { Manga } from './manga/entities/manga.entity';
 import { GenreModule } from './genre/genre.module';
 import { Genre } from './genre/entities/genre.entity';
 import { JikanService } from './jikan/jikan.service';
-import { HttpModule } from '@nestjs/axios';
 import { AuteurModule } from './auteur/auteur.module';
 import { ThemeModule } from './theme/theme.module';
 import { DemographicModule } from './demographic/demographic.module';
 import { Auteur } from './auteur/entities/auteur.entity';
 import { Theme } from './theme/entities/theme.entity';
 import { Demographic } from './demographic/entities/demographic.entity';
-import { ConfigModule } from '@nestjs/config';
 import { TomesModule } from './tomes/tomes.module';
 import { Tome } from './tomes/entities/tome.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
     TypeOrmModule.forRoot({
-      type: 'mariadb',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'test',
-      database: 'mangastate',
-      entities: [Manga,Genre,Auteur,Theme,Demographic,Tome],
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [Manga, Genre, Auteur, Theme, Demographic, Tome],
       synchronize: true,
-      logging:true
+      logging: true,
     }),
     HttpModule,
     MangaModule,
