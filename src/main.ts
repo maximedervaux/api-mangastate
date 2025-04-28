d224893384d78f186aaf15747e5fe1d0d354a549
maximedervaux <maximeder.pro@gmail.com>
2025-04-28 21:11:32 +0200

    fix: remove unused Logger import from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
