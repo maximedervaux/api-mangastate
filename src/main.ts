d3eca218c9818e34d0516d621dac1fe99af847b2
maximedervaux <maximeder.pro@gmail.com>
2025-04-28 21:28:16 +0200

    fix: add credentials option to CORS configuration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,8 @@ async function bootstrap() {
     "origin": "*",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
-    "optionsSuccessStatus": 204
+    "optionsSuccessStatus": 204,
+    "credentials": true,
   });
 
   const config = new DocumentBuilder()
