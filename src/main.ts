6a19deac8fb1e0dd273a4e968ac7a2bc74c4c471
maximedervaux <maximeder.pro@gmail.com>
2025-04-28 22:05:43 +0200

    fix: update CORS configuration to allow all origins

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.enableCors({
-    origin: "https://front-mangastate.vercel.app",  
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
-    credentials: true, 
+    credentials: true,
   });
 
   const config = new DocumentBuilder()
@@ -18,11 +19,10 @@ async function bootstrap() {
     .setDescription('Api de MangAstate')
     .setVersion('1.0')
     .build();
-    
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3001, '0.0.0.0')
+  await app.listen(3001, '0.0.0.0');
   
-}
 bootstrap();
